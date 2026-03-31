655b6534c0ddb4f05e841c3c1d501499904b4057
ma <maruno.ilmara@gmail.com>
2026-03-30 21:11:54 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import HomePage from "./src/pages/HomePage"
+
+const App = () => {
+  return (
+    <HomePage />
+  );
+};
+
+export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const HomePage = () => {
+  return (
+    <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
+
+      <Text> Ola, mundo!</Text>
+    
+    </View>
+  );
+};
+
+export default HomePage;
